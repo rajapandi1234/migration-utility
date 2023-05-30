8c5b61f2106fd1bde7324a3f9c3a9aa1c10760f5
Balaji <74903654+balaji-alluru@users.noreply.github.com>
2023-05-30 11:11:13 +0530

    updated LastSyncRepository

--- pms-115-120/utility/src/main/java/io/mosip/pms/ida/dao/LastSyncRepository.java
+++ pms-115-120/utility/src/main/java/io/mosip/pms/ida/dao/LastSyncRepository.java
@@ -15,6 +15,9 @@ public interface LastSyncRepository extends JpaRepository<LastSync, LocalDateTim
 	@Query(value = "select t.last_sync,t.cr_by,t.cr_dtimes,t.upd_by,t.upd_dtimes FROM pms.last_sync_time_stamp t where t.last_sync=(select max(t.last_sync) FROM pms.last_sync_time_stamp t)", nativeQuery = true)
 	LastSync findLastSync();
 	
-	@Query(value = "delete from pms.last_sync_time_stamp where where last_sync=(select min(last_sync) FROM pms.last_sync_time_stamp) ", nativeQuery = true)
+	@Query(value = "delete from pms.last_sync_time_stamp where last_sync=(select min(last_sync) FROM pms.last_sync_time_stamp) ", nativeQuery = true)
 	void deleteOldEntry();
+	
+	@Query(value = "update pms.last_sync_time_stamp last_sync=?1 where last_sync=(select min(last_sync) FROM pms.last_sync_time_stamp) ", nativeQuery = true)
+	void updatePreviousEntry(LocalDateTime lastSync);
 }
